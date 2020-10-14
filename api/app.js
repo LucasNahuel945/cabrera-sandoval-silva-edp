ae9933a76b55ee948d3310fa0f4fc1e64b7f4c29
lucasnahuel945 <lukaz945@gmail.com>
2020-10-14 15:30:30 -0300

    Modificaciones en app.js y ./bin/www
    Se agregaron comentarios y se modificó el código para que sea más claro

--- api/app.js
+++ api/app.js
@@ -1,36 +1,39 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var carrerasRouter = require('./routes/carreras');
-var materiasRouter = require('./routes/materias')
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const carreras = require('./routes/carreras');
+const materias = require('./routes/materias');
+const institutos = require('./routes/institutos');
+const app = express();
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+// Configuracion carpeta de vistas y motor de plantillas (pug en este caso)
+app.set('views', path.join(__dirname, 'views')); // carpeta => ./views
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
-app.use(express.json());
+// Middlewares
+app.use(morgan('dev')); // Muestra informacion de solicitudes al servidor
+app.use(express.json()); // Permite trabajar con JSON
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); // direccion de la carpeta publica
 
-app.use('/carreras', carrerasRouter);
-app.use('/materias', materiasRouter);
+// Rutas
+app.use('/api/carreras', carreras);
+app.use('/api/materias', materias);
+app.use('/api/institutos', institutos);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Manejador de errores
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = (req.app.get('env') === 'development') ? err : {};
 
   // render the error page
   res.status(err.status || 500);
